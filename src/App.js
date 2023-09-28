629fb71192f8a2daa14e92f2d2165e7de5c14186
ubunturoxshacker <>
2023-09-28 13:35:39 +0530

    pipe-test6

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title"> 👋  and welcome </h1>
         </header>
         <p className="App-intro">
-          To get started run npm start when in dev env.
+          To get started type npm start in cli when in dev env.
         </p>
       </div>
     );
