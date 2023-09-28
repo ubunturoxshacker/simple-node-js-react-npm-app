a87cc4273caa1165c67593a7fb94824606b98bd7
ubunturoxshacker <>
2023-09-28 13:26:20 +0530

    pipe-test4

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title"> 👋  and welcome </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started run npm start
         </p>
       </div>
     );
